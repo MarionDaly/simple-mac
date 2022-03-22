de0daac5a61bcb9915c9e0bc993126ce43121ddc
Marion Patricia Daly <MarionPatriciaDaly@gmail.com>
2022-03-22 11:16:03 -0400

    Update mac-setup.sh
    Update script

--- mac-setup.sh
+++ mac-setup.sh
@@ -6,18 +6,18 @@ brew install coreutils
 brew tap homebrew/dupes
 brew install binutils
 brew install diffutils
-brew install ed --with-default-names
-brew install findutils --with-default-names
+brew install ed 
+brew install findutils 
 brew install gawk
-brew install gnu-indent --with-default-names
-brew install gnu-sed --with-default-names
-brew install gnu-tar --with-default-names
-brew install gnu-which --with-default-names
+brew install gnu-indent
+brew install gnu-sed
+brew install gnu-tar
+brew install gnu-which
 brew install gnutls
-brew install grep --with-default-names
+brew install grep
 brew install gzip
 brew install screen
-brew install wdiff --with-gettext
+brew install wdiff
 brew install cask
 brew install git
 brew install wget
@@ -34,29 +34,20 @@ pip install speedtest-cli
 brew install wifi-password
 brew install qlcolorcode qlstephen qlmarkdown quicklook-json webpquicklook suspicious-package quicklookase qlvideo qlImageSize
 brew install file-formula
-brew install git
 brew install openssh
 brew install perl
 brew install rsync
 brew install svn
 brew install unzip
 brew install vim --override-system-vi
-brew install macvim --override-system-vim --custom-system-icons
 brew install zsh
-brew cask install betterzip 	
-brew cask install webpquicklook  
-brew cask install firefox 
-brew cask install google-chrome 
-brew cask install iterm2 
-brew cask install visual-studio-code
-brew cask install github
-brew cask install appcleaner 
-brew cask install sublime-text
-brew cask install transmission 
-brew cask install vlc
-brew cask install spotify 
-brew cask install spotify-notiications
-brew cask install java
-brew cask install macvim
-brew cleanup -force
+brew install python3
+brew install --cask betterzip 	
+brew install --cask webpquicklook  
+brew install --cask github
+brew install --caskappcleaner 
+brew install --cask sublime-text
+brew install --casktransmission 
+brew install --cask java
+brew cleanup
 rm -f -r /Library/Caches/Homebrew/*
